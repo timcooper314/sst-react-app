c5a8e540eec895dc082cf65f260876da71296bcb
Tim Cooper <timcooper314@gmail.com>
2021-09-16 18:12:56 +1000

    Created a Track component, within Tracks. Moved tracks to app.js.

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import { useState } from 'react'
 import Track from './Track'
 
 
-const Tracks = () => {
-    const [tracks, setTracks] = useState([
-        {
-            id: 1,
-            artist: "x",
-            track: "y",
-        },
-        {
-            id: 2,
-            artist: "a",
-            track: "b",
-        }
-    ])
+const Tracks = ({tracks}) => {
     return (
         <>
             {tracks.map((track) => (
-                <h3 key={track.id}> {track.artist} </h3>
+                <Track key={track.id} track={track}/>
             ))}
         </>
     )
