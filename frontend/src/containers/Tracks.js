27c83a14ee9d5233819daf9dda7091feccc709d5
Tim Cooper <timcooper314@gmail.com>
2022-02-05 17:22:45 +1100

    Fetch the track dates and display them in a drop down button. Use useState to track the selected date.

--- frontend/src/containers/Tracks.js
+++ frontend/src/containers/Tracks.js
@@ -5,9 +5,28 @@ import TracksList from '../components/TracksList';
 import { API } from "aws-amplify";
 
 export default function Tracks() {
+    const [trackDates, setTrackDates] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(trackDates[0]);
     const [showTracks, setShowTracks] = useState(false);
     const [tracks, setTracks] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    // Get the dates only once:
+    useEffect(() => {
+        const getTrackDates = async () => {
+            const trackDatesFromCloud = await fetchTrackDates()
+            setTrackDates(trackDatesFromCloud)
+            console.log("Setting default date: " + trackDatesFromCloud[0])
+            setSelectedDate(trackDatesFromCloud[0])
+        }
+        getTrackDates()
+    }, []);
+
+    const fetchTrackDates = async () => {
+        console.log("Fetching track dates data...")
+        setIsLoading(true);
+        return API.get("topmusic", "/topmusic/tracks/dates");
+    }
 
     useEffect(() => {
         const getTracks = async () => {
@@ -31,6 +50,9 @@ export default function Tracks() {
                 onAdd={() => setShowTracks(!showTracks)}
                 showAdd={showTracks}
                 isLoading={isLoading}
+                datesList={trackDates}
+                onDateSelect={(date) => setSelectedDate(date)}
+                selectedDate={selectedDate}
             />
             {showTracks && <TracksList tracks={tracks} />}
         </div>
